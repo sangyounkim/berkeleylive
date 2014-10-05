bdac5d399a11da4899f5adfabbd693fd4b74353e
EvanRocha <evanr@eventbrite.com>
2014-10-05 04:41:39 -0700

    Tested function for showing tweets

--- public/js/map.js
+++ public/js/map.js
@@ -2,6 +2,7 @@
 var initialLocation;
 var siberia = new google.maps.LatLng(60, 105);
 var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+var tweets = [];
 var browserSupportFlag =  new Boolean();
 
 function initialize() {
@@ -41,14 +42,46 @@ function initialize() {
   }
 
   function setMarker(map, myLatLng){
-    // src = http://www.keemarketing.it/new/wp-content/uploads/ping.jpg;
-    // var iconBase = $('<img src="' + src + '" />');
-    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    src = './picker.png'
+    var iconBase = $('<img src="' + src + '" />');
+    // var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
     var marker = new google.maps.Marker({
       position: myLatLng,
       map: map,
-      icon: iconBase + 'schools_maps.png'
+      // icon: iconBase + 'schools_maps.png'
+      icon: src
     });
-  }  
+
+    setTweet(map, myLatLng);
+  }
+
+  function setTweet(map, LatLng){
+    var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+        '<div id="bodyContent">'+
+        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: LatLng,
+        map: map,
+        title: 'Uluru (Ayers Rock)'
+    });
+
+    infowindow.open(map,marker); 
+  } 
 }
 google.maps.event.addDomListener(window, "load", initialize);
